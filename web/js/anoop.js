384b0f4c99da4b6ab7d7d17da48dd6990f4f041d
Anoop S <anoop.santhanam@gmail.com>
2018-01-16 23:23:27 +0530

    Adding effects

--- web/js/anoop.js
+++ web/js/anoop.js
@@ -7,3 +7,34 @@ Fun fact: Putting this up EXACTLY
 Coincidence? I think not!
 ---------------------------------*/
 var app=angular.module("anoop",[]);
+app.controller("home",function($scope,$http,$compile){
+    $scope.messages=["Hi!","My name is Anoop Santhanam."];
+    $scope.position=null;
+    $scope.displayText=function(){
+        if(validate($scope.position)){
+            var text=$scope.messages[$scope.position];
+            $("#maintext").html('');
+            var done=$scope.spellText(0);
+            if(done=="DONE"){
+                $scope.position+=1;
+                setTimeout(function(){
+                    $scope.displayText();
+                },5000);
+            }
+        }
+    };
+    $scope.spellText=function(pos){
+        var text=$scope.messages[$scope.position];
+        if(pos>=text.length){
+            return "DONE";
+        }
+        else{
+            var letter=text[pos];
+            $("#maintext").append(letter);
+            pos+=1;
+            setTimeout(function(){
+                return $scope.spellText(pos);
+            },200);
+        }
+    };
+});
