ff20997114daa3a5bf106497b1aaa48c8c1ac873
Anoop S <anoop.santhanam@gmail.com>
2018-01-17 03:08:05 +0530

    Changed speed

--- web/js/anoop.js
+++ web/js/anoop.js
@@ -54,7 +54,7 @@ app.controller("home",function($scope,$http,$compile){
             pos+=1;
             setTimeout(function(){
                 $scope.spellText(pos);
-            },50);
+            },75);
         }
     };
     $scope.nextMessage=function(){
