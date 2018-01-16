abb58793e6bd79003f351871263b909cd4bfd8e6
Anoop S <anoop.santhanam@gmail.com>
2018-01-16 17:40:11 +0530

    Setting up layout

---
+++ web/js/anoop.js
@@ -0,0 +1,9 @@
+/*---------------------------------
+Author: Anoop Santhanam
+Date Created: 16/1/18 17:38
+Comments: Main JS file.
+Fun fact: Putting this up EXACTLY 
+6 years after I put Binox up. 
+Coincidence? I think not!
+---------------------------------*/
+var app=angular.module("anoop",[]);
