efcaf99b82a7a0c355537d5eca3db67594782ee5
Anoop S <anoop.santhanam@gmail.com>
2018-01-17 10:15:43 +0530

    Adding my achievements

--- web/js/anoop.js
+++ web/js/anoop.js
@@ -14,7 +14,11 @@ app.controller("home",function($scope,$http,$compile){
     },{
         text: "And this is a summary of my work experience!",
         images: ["images/freelancer.jpg","images/binox.png","images/spectral_logo.png","images/dust-logo.png"]
-    },"Now, the fun part ...","Let's go through every single project I've built from the start of time, shall we?","Yeah, you don't really have a choice."];
+    },"Now, the fun part ...","Let's go through every single project I've built from the start of time, shall we?","Yeah, you don't really have a choice. I'll make sure it's fun though!",{
+        text: "Binox Web Services",
+        description: "To facilitate development of projects outsourced to us, we launched Binox Web Services to not only design and develop solutions, but to maintain web servers, design graphics content, and host the web content onto the internet. While I did all of the coding, I occasionally also designed logos and icons using Adobe Photoshop.",
+        url: "https://binox.me/binoxservice"
+    }];
     $scope.position=null;
     $scope.displayText=function(){
         if($scope.position!=null){
@@ -31,12 +35,23 @@ app.controller("home",function($scope,$http,$compile){
     $scope.spellText=function(pos){
         var text=$scope.messages[$scope.position];
         var images=null;
+        var desc=null;
+        var url=null;
         if(typeof text=="object"){
             images=text.images;
+            if(validate(text.description)){
+                desc=text.description;
+            }
+            if(validaye(text.url)){
+                url=text.url;
+            }
             text=text.text;
         }
         if(pos>=text.length){
             $scope.position+=1;
+            if(validate(desc)){
+                $("#content").html('<p>'+desc+'</p>');
+            }
             if(validate(images)){
                 var list='<div class="row">';
                 for(var i=0;i<images.length;i++){
@@ -44,7 +59,10 @@ app.controller("home",function($scope,$http,$compile){
                     list+='<div class="col-sm-6 text-center"><img src="'+image+'" class="img-thumbnail" style="width:200px;margin-top:20px;"></div>';
                 }
                 list+='</div>';
-                $("#content").html(list);
+                $("#content").append(list);
+            }
+            if(validate(url)){
+                $("#content").append('<iframe src="'+url+'" name="iframe" id="iframe" frameborder=0 style="width:100%;height:400px;"></iframe>');
             }
             $("#maintext").append('<span class="blinking-cursor">|</span>');
         }
